b634425b65722a19cf0f7d776e55cdfca66efcfe
CheekyRishi <rishabhshrivastava2007@gmail.com>
2026-01-13 20:06:10 +0530

    Update resnet models

--- src/models/resnet.py
+++ src/models/resnet.py
@@ -4,20 +4,20 @@ import torchvision
 from torch import nn
 import timm
 
-def resnet50(num_classes=10):
-    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
+def resnet50(num_classes=10):
     # 1. Load weights and model
     weights = torchvision.models.ResNet50_Weights.DEFAULT
     model = torchvision.models.resnet50(weights=weights).to(device)
     automatic_transform = weights.transforms()
     # 2. Freeze all base layers
-    for param in model.features.parameters():
+    for param in model.parameters():
         param.requires_grad = False
     
     updated_classifier = nn.Sequential(
         nn.Dropout(inplace=True,p=0.2),
-        nn.Linear(in_features=1280,out_features=num_classes,bias=True)
+        nn.Linear(in_features=2048,out_features=num_classes,bias=True)
     ).to(device)
 
     model.fc = updated_classifier
@@ -26,7 +26,6 @@ def resnet50(num_classes=10):
 
 
 def resnet26(num_classes=10):
-    device = 'cuda' if torch.cuda.is_available() else 'cpu'
     
     model = timm.create_model('resnet26', pretrained=True).to(device)
     
@@ -47,7 +46,6 @@ def resnet26(num_classes=10):
     return model, transforms
 
 def resnet18(num_classes=10):
-    device = 'cuda' if torch.cuda.is_available() else 'cpu'
     
     weights = torchvision.models.ResNet18_Weights.DEFAULT
     model = torchvision.models.resnet18(weights=weights).to(device)
