663f42b284c17242b21293f0d10b3e82f8812c55
CheekyRishi <rishabhshrivastava2007@gmail.com>
2026-01-20 13:30:08 +0530

    Add epoch tracking

--- src/utils/early_stopping.py
+++ src/utils/early_stopping.py
@@ -2,21 +2,23 @@ import torch
 import numpy as np
 
 class EarlyStopping:
-    def __init__(self, patience=10, min_delta=0.0, save_path="best_model.pt"):
+    def __init__(self, patience=10, min_delta=0.0):
         self.patience = patience
         self.min_delta = min_delta
-        self.save_path = save_path
 
-        self.best_loss = np.inf
+        self.best_loss = float("inf")
+        self.best_epoch = None
         self.counter = 0
         self.should_stop = False
 
-    def step(self, current_loss, model):
+    def step(self, current_loss, epoch):
         if current_loss < self.best_loss - self.min_delta:
             self.best_loss = current_loss
+            self.best_epoch = epoch
             self.counter = 0
-            torch.save(model.state_dict(), self.save_path)
         else:
             self.counter += 1
             if self.counter >= self.patience:
                 self.should_stop = True
+
+
